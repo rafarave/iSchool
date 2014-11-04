1c49d4782a3115d69bd5aa2b16afe480a2b770ae
rafarave <rafael.ravena@gmail.com>
2014-11-04 18:30:45 -0200

    create fucking project
    wow! very create
    application many configure
    extremely solution

---
+++ iSchool.Api/iSchool.Api.ServiceInterface/MyServices.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ServiceStack;
+using iSchool.Api.ServiceModel;
+
+namespace iSchool.Api.ServiceInterface
+{
+	public class MyServices : Service
+	{
+		public object Any(Hello request)
+		{
+			return new HelloResponse { Result = "Hello, {0}!".Fmt(request.Name) };
+		}
+
+		public object Get(AlunoGet request)
+		{
+			//Aluno aluno = new Aluno().ObterAlunoPorId(request.Disciplina, request.Turma);
+			//return aluno;
+			//return new string[]
+			//{
+			//	Aprovado = aluno.Aprovado,
+			//	Nome = aluno.Nome,
+			//	NomePai = aluno.Pai.Nome
+			//};
+			return "MyAss";
+		}
+	}
+}
---
+++ iSchool.Api/iSchool.Api.ServiceModel/Hello.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ServiceStack;
+
+namespace iSchool.Api.ServiceModel
+{
+	public class HelloResponse
+	{
+		public string Result { get; set; }
+	}
+
+	[Route("/hello/{Name}")]
+	public class Hello : IReturn<HelloResponse>
+	{
+		public string Name { get; set; }
+	}
+
+}
