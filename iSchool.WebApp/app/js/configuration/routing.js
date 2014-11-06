aab779d3fa40ed75935b78f6ce813299e9cfbec9
Gabriel Ferreira <gabriel.leandrof@gmail.com>
2014-11-06 01:28:11 -0200

    ajustes web
    ajustes realizados no template, ainda em adaptação

--- iSchool.WebApp/app/js/configuration/routing.js
+++ iSchool.WebApp/app/js/configuration/routing.js
@@ -4,7 +4,7 @@ angular.module('iSchoolApp')
 .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
 
 	$routeProvider.when('/inicio', { templateUrl: 'app/partials/inicio.html', controller: 'inicioController' });
-	//$routeProvider.when('/busca', { templateUrl: 'app/partials/busca.html', controller: 'buscaController' });
+	$routeProvider.when('/comunicados', { templateUrl: 'app/partials/comunicados.html', controller: 'comunicadosController' });
 	//$routeProvider.when('/busca/item', { templateUrl: 'app/partials/parque-praca.html', controller: 'parquePracaController' });//item:codigo
 	$routeProvider.otherwise({ redirectTo: '/inicio' });
 
---
+++ iSchool.WebApp/app/js/controllers/comunicadosController.js
@@ -0,0 +1,10 @@
+﻿
+angular.module('iSchoolApp.controllers')
+
+.controller('comunicadosController', function ($scope, $window/*, testeService*/) {
+
+	//testeService.get().success(function (data) {
+	//	$scope.itens = data;
+	//});
+
+});
