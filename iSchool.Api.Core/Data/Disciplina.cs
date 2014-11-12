0974e6d53d3600235042b794e87d4ca12ce4461f
rafarave <rafael.ravena@gmail.com>
2014-11-12 17:06:14 -0200

    Vê se é isso

---
+++ iSchool.Api.Core/Data/Disciplina.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iSchool.Api.Core.Data
+{
+	public class Disciplina : DataAccessBase<Model.Disciplina>
+	{
+		public override System.Data.Entity.Infrastructure.DbEntityEntry GetCurrent(Model.Disciplina entity)
+		{
+			return context.Entry(entity);
+		}
+
+		public override void Insert(Model.Disciplina entity)
+		{
+			context.Disciplinas.Add(entity);
+			context.SaveChanges();
+		}
+
+		protected override List<Model.Disciplina> GetCollection()
+		{
+			return context.Disciplinas.ToList();
+		}
+	}
+}
---
+++ iSchool.Api.ServiceInterface/Aluno.cs
@@ -0,0 +1,37 @@
+﻿using iSchool.Api.ServiceModel;
+using ServiceStack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using model = iSchool.Api.Core.Model;
+using data = iSchool.Api.Core.Data;
+
+namespace iSchool.Api.ServiceInterface
+{
+	public class Aluno : Service
+	{
+		public model.Aluno Get(GetAluno request)
+		{
+			data.Aluno aluno = new data.Aluno();
+			return aluno.GetElement(request.Id);
+		}
+		public List<model.Aluno> Get(GetAlunos request)
+		{
+			return new data.Aluno().GetCollection(0);
+		}
+		public void Insert(NewAluno request)
+		{
+			new data.Aluno().Insert(request.Aluno);
+		}
+		public void Update(UpdateAluno request)
+		{
+			new data.Aluno().Update(request.Aluno);
+		}
+		public void Delete(DeleteAluno request)
+		{
+			new data.Aluno().Delete(request.Id);
+		}
+	}
+}
