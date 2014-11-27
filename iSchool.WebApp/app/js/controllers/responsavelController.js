a0031d25a8fee89aa13396a7c98a8e2e07a4256f
Gabriel Ferreira <gabriel.leandrof@gmail.com>
2014-11-27 17:02:11 -0200

    responsavel
    praticamente pronto, aguardando o serviço de get

--- iSchool.WebApp/app/js/controllers/responsavelController.js
+++ iSchool.WebApp/app/js/controllers/responsavelController.js
@@ -1,19 +1,40 @@
 ﻿
 angular.module('iSchoolApp.controllers')
 
-.controller('responsavelController', function ($scope, $routeParams, alunosService, $filter) {
+.controller('responsavelController', function ($scope, $routeParams, $filter, alunosService, responsavelService) {
 
 	$scope.alunoEntity = { Aluno: {} };
+	var IdAluno = $routeParams.id;
 
 	$scope.init = function () {
 		alunosService.getAluno($routeParams.id).success(function (data) {
 			$scope.alunoEntity.Aluno = data;
 			$scope.alunoEntity.Aluno.Nascimento = $filter('momentParseDateASPNetJSON')(data.Nascimento);
-
-			responsavelService.getPorAluno().success(function (data) {
-
-			});
+			$scope.obtemResponsaveisDoAluno(IdAluno);
 		});
+	};
+
+	$scope.salvaResponsavel = function () {
+		var responsavel = {
+			Responsavel: {
+				Nome: $scope.txtNome,
+				Email: $scope.txtEmail
+			}
+		};
+		responsavelService.postResponsavel(responsavel).success(function (data) {
+			$scope.obtemResponsaveisDoAluno(IdAluno);
+		});
+	};
+
+	$scope.excluiResponsavel = function (id) {
+		responsavelService.deleteResponsavel(id).success(function (data) {
+			$scope.obtemResponsaveisDoAluno(IdAluno);
+		});
+	};
+
+	//TODO: descomentar apos a criação do serviço
+	$scope.obtemResponsaveisDoAluno = function (id) {
+		//responsavelService.getPorAluno().success(function (data) { });
 	};
 
 	$scope.init();
